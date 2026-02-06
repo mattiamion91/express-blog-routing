b2e27d80a78b67eefcf280eb19010f12beb13fd9
Mattia Mion <mattia.mion@gmail.com>
2026-02-06 17:05:06 +0100

    completamento bonus {usando parseInt() per rendere una stringa un numero}

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ app.get('/', (req, res) => { //prima rotta preincipale
     //res.send('<h1>Server del mio blog</h1>') //il type di default é 'html'
 })
 
+//istanza rotte posts
+app.use('/posts', rottePosts)
+
 // Avvia il server e lo mette in ascolto per richieste HTTP sulla porta specificata
 app.listen(port, () => {
     console.log(`esempio di app in ascolto sulla porta ${port}`); //tempalte litteral perche se in ambiente js
--- routes/posts.js
+++ routes/posts.js
@@ -2,31 +2,37 @@
 const express = require('express');
 //creo istanza oggetto rotte
 const router = express.Router();
+//importo sita posts da posts.js
+const listaPosts = require('../data/postsList')
 
 //rotte di crud
 // index
-router.get('/posts', function (req, res) {
-    res.send('Lista delle pizze');
+router.get('/', function (req, res) {
+    res.json({numeroPosts: listaPosts.length, listaPosts});
 });
 // show
-router.get('/posts/:id', function (req, res) {
-    res.send('Dettagli della pizza ' + req.params.id);
+router.get('/:id', function (req, res) {
+    //rendo utilizzabile singolo post usando id
+    const postById = parseInt(req.params.id) //uso parse int perche req.params.id mi sertitusce una stringa e io ho bisogna di un numero per rendere vera l'uguaglianza stretta (non ci sarei mai arrivato da solo!)
+    //cerco post specifico usando metodo 'find' e usando id specifico recuerparo con req.params.id
+    const showById = listaPosts.find((post) => post.id === postById) //sintasssi meootdo find copiata da mdn
+    res.json(showById);
 });
 // store
-router.post('/posts', function (req, res) {
-    res.send('Creazione nuova pizza');
+router.post('/', function (req, res) {
+    res.send('Creazione nuovo post');
 });
 // update
-router.put('/posts/:id', function (req, res) {
-    res.send('Modifica integrale della pizza ' + req.params.id);
+router.put('/:id', function (req, res) {
+    res.send('Modifica integrale del post ' + req.params.id);
 });
 // modify
-router.patch('/posts/:id', function (req, res) {
-    res.send('Modifica parziale della pizza ' + req.params.id);
+router.patch('/:id', function (req, res) {
+    res.send('Modifica parziale del post ' + req.params.id);
 });
 // destroy
-router.delete('/posts/:id', function (req, res) {
-    res.send('Eliminazione della pizza ' + req.params.id);
+router.delete('/:id', function (req, res) {
+    res.send('Eliminazione del post ' + req.params.id);
 });
 
 //esporto istanza rootte
