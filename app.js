642bf9f3d13b18c14bbba582f82524e3789b3177
Mattia Mion <mattia.mion@gmail.com>
2026-02-06 16:28:08 +0100

    imposto istanza oggetto rotte, sposto rotte crud in routes/posts.js

--- app.js
+++ app.js
@@ -10,32 +10,6 @@ app.get('/', (req, res) => { //prima rotta preincipale
     //res.send('<h1>Server del mio blog</h1>') //il type di default é 'html'
 })
 
-//rotte di crud
-// index
-app.get('/posts', function (req, res) {
-    res.send('Lista delle pizze');
-});
-// show
-app.get('/posts/:id', function (req, res) {
-    res.send('Dettagli della pizza ' + req.params.id);
-});
-// store
-app.post('/posts', function (req, res) {
-    res.send('Creazione nuova pizza');
-});
-// update
-app.put('/posts/:id', function (req, res) {
-    res.send('Modifica integrale della pizza ' + req.params.id);
-});
-// modify
-app.patch('/posts/:id', function (req, res) {
-    res.send('Modifica parziale della pizza ' + req.params.id);
-});
-// destroy
-app.delete('/posts/:id', function (req, res) {
-    res.send('Eliminazione della pizza ' + req.params.id);
-});
-
 // Avvia il server e lo mette in ascolto per richieste HTTP sulla porta specificata
 app.listen(port, () => {
     console.log(`esempio di app in ascolto sulla porta ${port}`); //tempalte litteral perche se in ambiente js
---
+++ routes/posts.js
@@ -0,0 +1,30 @@
+//importo express
+const express = require('express');
+//creo istanza oggetto rotte
+const router = express.Router();
+
+//rotte di crud
+// index
+router.get('/posts', function (req, res) {
+    res.send('Lista delle pizze');
+});
+// show
+router.get('/posts/:id', function (req, res) {
+    res.send('Dettagli della pizza ' + req.params.id);
+});
+// store
+router.post('/posts', function (req, res) {
+    res.send('Creazione nuova pizza');
+});
+// update
+router.put('/posts/:id', function (req, res) {
+    res.send('Modifica integrale della pizza ' + req.params.id);
+});
+// modify
+router.patch('/posts/:id', function (req, res) {
+    res.send('Modifica parziale della pizza ' + req.params.id);
+});
+// destroy
+router.delete('/posts/:id', function (req, res) {
+    res.send('Eliminazione della pizza ' + req.params.id);
+});
